ef25fc53f476ac5e7c534ffb34387082ace6f232
Jonathan Armknecht <jonathan.armknecht@gmail.com>
2019-01-11 16:49:03 -0700

    value show up just need to calculate it

--- app/src/main/java/com/example/jonathan/diy_slider/MainActivity.java
+++ app/src/main/java/com/example/jonathan/diy_slider/MainActivity.java
@@ -2,8 +2,10 @@ package com.example.jonathan.diy_slider;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.MotionEvent;
 import android.widget.FrameLayout;
 import android.view.View;
+import android.widget.TextView;
 
 public class MainActivity extends AppCompatActivity {
 
@@ -13,12 +15,18 @@ public class MainActivity extends AppCompatActivity {
         setContentView(R.layout.activity_main);
 
         //instantiate my two thumbed seekbar.
-        MySeekBar mySeekBar = new MySeekBar(this);
+        final MySeekBar mySeekBar = new MySeekBar(this);
 
         // add it to the layout.
         FrameLayout layout = (FrameLayout) findViewById (R.id.seekbar_placeholder);
         layout.addView(mySeekBar);
 
-
+        mySeekBar.setOnTouchListener(new View.OnTouchListener() {
+            @Override
+            public boolean onTouch(View view, MotionEvent e) {
+                ((TextView)view.getRootView().findViewById(R.id.sliderValue)).setText(String.valueOf(mySeekBar.getCurrValue()));
+                return false;
+            }
+        });
     }
 }
